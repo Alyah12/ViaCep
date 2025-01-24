30a9201c83c0f81240c7c424642f62e629775879
Heberth Pólvora <86444094+Alyah12@users.noreply.github.com>
2025-01-24 12:21:54 -0300

    feat(Adição do projeto): API ViaCep

---
+++ FilmesCatalago/Controllers/CepCotroller.cs
@@ -0,0 +1,43 @@
+﻿using FilmesCatalago.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesCatalago.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class CepCotroller : ControllerBase
+{
+
+    private readonly ILogger<CepCotroller> _logger;
+    private readonly HttpClient _httpClient;
+
+    public CepCotroller(ILogger<CepCotroller> logger, HttpClient httpClient)
+    {
+        _logger = logger;
+        _httpClient = httpClient;
+    }
+
+    [HttpGet("{cep}")]
+    public async Task<ActionResult<CepDTO>> GetCep(string cep)
+    {
+        try
+        {
+            HttpClient httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri("https://viacep.com.br")
+            };
+
+            var response = await httpClient.GetAsync($"/ws/{cep}/json/");
+
+            response.EnsureSuccessStatusCode();
+            CepDTO json = await response?.Content.ReadFromJsonAsync<CepDTO>();
+            return Ok(json);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Erro ao Obter CEP, e.InnerException: {e}");
+        }
+        return BadRequest();
+    }
+}
