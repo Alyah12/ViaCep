3c350549e17d69e64a773c7b1b9125c52e0c8fda
Heberth Pólvora <86444094+Alyah12@users.noreply.github.com>
2025-01-25 02:06:49 -0300

    fix(Edição): Mudança de nome do Projeto

---
+++ APICep/Controllers/CepController.cs
@@ -0,0 +1,43 @@
+﻿using APICep.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICep.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CepController : ControllerBase
+{
+    private readonly HttpClient _httpClient;
+
+    private readonly ILogger<CepController> _logger;
+
+    public CepController(ILogger<CepController> logger, HttpClient httpClient)
+    {
+        _logger = logger;
+        _httpClient = httpClient;
+    }
+
+    [HttpGet("{cep}")]
+    public async Task<ActionResult<CepDTO>> GetCep(string cep)
+    {
+        try
+        {
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://viacep.com.br")
+            };
+
+            var response = await httpClient.GetAsync($"/ws/{cep}/json/");
+
+            response.EnsureSuccessStatusCode();
+            var json = await response?.Content.ReadFromJsonAsync<CepDTO>();
+            return Ok(json);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Erro ao Obter CEP, e.InnerException: {e}");
+        }
+
+        return BadRequest();
+    }
+}
